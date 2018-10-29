aa06e1d80a262b81b1a6b7fdedff72362dfd5729
masato <masato@m-mbp.local>
2018-10-29 10:58:29 +0800

    Initial Commit

---
+++ WebScraping_Kanna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebScraping_Kanna
+//
+//  Created by masato on 29/10/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
